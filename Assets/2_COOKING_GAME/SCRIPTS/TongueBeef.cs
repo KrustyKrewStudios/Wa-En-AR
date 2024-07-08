7bf2f4c4644009309fd7cf186453490c2c756e74
curtislow777 <116061120+curtislow777@users.noreply.github.com>
2024-07-08 23:35:49 +0800

    ok the prefabs are sus

--- Assets/2_COOKING_GAME/SCRIPTS/TongueBeef.cs
+++ Assets/2_COOKING_GAME/SCRIPTS/TongueBeef.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class TongueBeef : BeefBase
 {
-    public enum BeefType { Karubi }
+    public enum BeefType { Tongue }
     //private BeefType beefType = BeefType.Karubi;
 
     protected override void Start()
