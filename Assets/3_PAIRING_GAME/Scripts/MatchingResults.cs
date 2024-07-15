a3933cbf427173f581f2af47ef7cd3413d30e896
bhoomikaxxx <102478503+bhoomikaxxx@users.noreply.github.com>
2024-07-15 12:10:24 +0800

    Adding models & textures

---
+++ Assets/3_PAIRING_GAME/Scripts/MatchingResults.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MatchingResults : MonoBehaviour
+{
+    private HashSet<string> detectedItems = new HashSet<string>();
+
+    public string[] beefItems = { "Chuck", "Sirloin", "Ribeye", "ShortRib", "BeefTongue" };
+    public string[] drinkItems = { "Campfire", "Martini", "Sake", "OldFashioned" };
+
+    // Dictionary to store ratings for each combination
+    private Dictionary<string, int> comboRatings = new Dictionary<string, int>();
+
+    public TMP_Text ratingText;
+
+    void Start()
+    {
+        //Initialize comboRatings with your ratings
+        comboRatings.Add("Chuck_Campfire", 8);
+        comboRatings.Add("Chuck_Martini", 6);
+        comboRatings.Add("Chuck_Sake", 8);
+        comboRatings.Add("Chuck_OldFashioned", 6);
+        comboRatings.Add("Sirloin_Campfire", 8);
+        comboRatings.Add("Sirloin_Martini", 9);
+        comboRatings.Add("Sirloin_Sake", 7);
+        comboRatings.Add("Sirloin_OldFashioned", 7);
+        comboRatings.Add("Ribeye_Campfire", 10);
+        comboRatings.Add("Ribeye_Martini", 7);
+        comboRatings.Add("Ribeye_Sake", 6);
+        comboRatings.Add("Ribeye_OldFashioned", 8);
+        comboRatings.Add("ShortRib_Campfire", 6);
+        comboRatings.Add("ShortRib_Martini", 5);
+        comboRatings.Add("ShortRib_Sake", 7);
+        comboRatings.Add("ShortRib_OldFashioned", 9);
+        comboRatings.Add("BeefTongue_Campfire", 5);
+        comboRatings.Add("BeefTongue_Martini", 5);
+        comboRatings.Add("BeefTongue_Sake", 5);
+        comboRatings.Add("BeefTongue_OldFashioned", 5);
+
+        Debug.Log("Combo Initialized");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger script working");
+
+        string tag = other.tag;
+        detectedItems.Add(tag);
+
+        //CheckForCombinations();
+    }
+
+    /*void OnTriggerExit(Collider other)
+    {
+        string tag = other.tag;
+        detectedItems.Remove(tag);
+    }*/
+
+    public void CheckForCombinations()
+    {
+        foreach (string beef in beefItems)
+        {
+            foreach (string drink in drinkItems)
+            {
+                if (detectedItems.Contains(beef) && detectedItems.Contains(drink))
+                {
+                    Debug.Log("Rated!");
+                    RatePairing(beef, drink);
+                    // Optionally reset the state after rating
+                    //detectedItems.Remove(beef);
+                    //detectedItems.Remove(drink);
+                }
+            }
+        }
+    }
+
+    private void RatePairing(string beef, string drink)
+    {
+        string comboKey = $"{beef}_{drink}";
+        if (comboRatings.TryGetValue(comboKey, out int rating))
+        {
+            Debug.Log("Giving Rating");
+            string ratingString = $"Rating for pairing {beef} with {drink}: {rating}";
+
+            Debug.Log($"Rating for pairing {beef} with {drink}: {rating}");
+
+            if (ratingText != null)
+            {
+                ratingText.text = ratingString;
+            }
+        }
+        else
+        {
+            Debug.Log($"No rating found for pairing {beef} with {drink}");
+            string ratingString = $"No rating found for pairing {beef} with {drink}";
+
+            if (ratingText != null)
+            {
+                ratingText.text = ratingString;
+            }
+        }
+    }
+}
---
+++ Assets/3_PAIRING_GAME/Scripts/PairingGame.cs
@@ -0,0 +1,169 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PairingGame : MonoBehaviour
+{
+    //Declaration of values
+    public GameObject wheel;
+    public GameObject beef1;
+    public GameObject beef2;
+    //public GameObject drink1;
+    //public string[] scriptNamesToDisable;
+
+
+    //Spin timer
+    public float spinDuration = 5f;
+    private bool isSpinning = false;
+    private float currentSpeed;
+    private float spinTimer;
+
+    //Results
+    private string resultToShow = " "; 
+    public TMP_Text resultText;
+
+    void Start()
+    {
+        beef1.SetActive(false);
+        beef2.SetActive(false);
+        //DisableScripts(beef1, scriptNamesToDisable);
+    }
+
+    /*void DisableScripts(GameObject gameObject, string[] scriptNames)
+    {
+        foreach (string scriptName in scriptNames)
+        {
+            gameObject.SendMessage("SetScriptDisabled", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    void EnableScripts(GameObject gameObject, string[] scriptNames)
+    {
+        foreach (string scriptName in scriptNames)
+        {
+            gameObject.SendMessage("SetScriptEnabled", SendMessageOptions.DontRequireReceiver);
+        }
+    }*/
+
+    void Update()
+    {
+        //Debug.Log("Updating");
+
+        if (isSpinning == true)
+        {
+            //drink1.transform.Translate(Vector3.one * currentSpeed * Time.deltaTime);
+
+            //Rotate the wheel
+            wheel.transform.Rotate(Vector3.up, currentSpeed * Time.deltaTime);
+
+            //Decrement the spin timer
+            spinTimer -= Time.deltaTime;
+
+            Debug.Log("Rotating");
+            //Debug.Log(spinDuration);
+            //Debug.Log(spinTimer);
+
+            //Check if spin duration has passed
+            if (spinTimer <= 0f)
+            {
+                isSpinning = false;
+                currentSpeed = 0f;
+                //Start coroutine to delay showing result
+                StartCoroutine(DelayedShowResult(1f));
+                Debug.Log("Start coroutine");
+            }
+            else
+            {
+                //Deceleration
+                currentSpeed -= 100f * Time.deltaTime;
+                if (currentSpeed <= 0f)
+                {
+                    isSpinning = false;
+                    currentSpeed = 0f;
+                    //Start coroutine to delay showing result
+                    StartCoroutine(DelayedShowResult(1f));
+                    Debug.Log("Start coroutine");
+                }
+            }
+        }
+    }
+
+    IEnumerator DelayedShowResult(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+
+        //Show the stored result after delay
+        resultText.text = resultToShow;
+        Debug.Log(resultToShow);
+
+        //Set active objects according to result
+        if (resultToShow == "Result: Beef")
+        {
+            beef1.SetActive(true);
+        }
+        if (resultToShow == "Result: 5")
+        {
+            beef2.SetActive(true);
+        }
+    }
+
+    public void SpinWheel()
+    {
+        if (!isSpinning)
+        {
+            //Start spinning
+            isSpinning = true;
+            currentSpeed = 1000f;
+            spinTimer = spinDuration;
+            Debug.Log("Spinning");
+
+            //Reset 
+            resultText.text = " ";
+            beef1.SetActive(false);
+            beef2.SetActive(false);
+        }
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTrigger");
+
+        //Only update resultToShow if the wheel is still spinning
+        if (isSpinning) 
+        {
+            Debug.Log("OnTrigger - Unchecked");
+
+            if (other.CompareTag("Mesh3"))
+            {
+                resultToShow = "Ribeye";
+                Debug.Log("Mesh3");
+            }
+            else if (other.CompareTag("Mesh4"))
+            {
+                resultToShow = "Sirloin";
+            }
+            else if (other.CompareTag("Mesh5"))
+            {
+                resultToShow = "Chuck";
+                Debug.Log("Mesh5");
+            }
+            else if (other.CompareTag("Mesh1"))
+            {
+                resultToShow = "Karubi";
+            }
+            else if (other.CompareTag("Mesh2"))
+            {
+                resultToShow = "Beef Tongue";
+            }
+            else
+            {
+                resultToShow = "Result: Nil";
+                Debug.Log("Nil");
+            }
+        }
+    }
+}
