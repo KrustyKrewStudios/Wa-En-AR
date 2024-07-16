6a1ead7dcb1ab1286ab3c37696b11e80e1afe241
curtislow777 <116061120+curtislow777@users.noreply.github.com>
2024-07-16 17:23:04 +0800

    i think im getting trolled

--- Assets/1_MENU/ARMenu.cs
+++ Assets/1_MENU/ARMenu.cs
@@ -1,3 +1,4 @@
+using Imagine.WebAR;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,59 @@ using UnityEngine;
 public class ARMenu : MonoBehaviour
 {
     public GameObject arCanvas;
+    public ARCamera arCamera;
+    public GameObject uiCanvas;
 
-    // Start is called before the first frame update
+    public GameObject searedWagyu;
+    public GameObject beefPlatter;
+    public GameObject tata;
+    public GameObject wagyuJyu;
+    public GameObject salad;
+
     void Start()
     {
+       arCamera = GetComponent<ARCamera>();
+    }
+
+    public void ArMode()
+    {
+        arCamera.UnpauseCamera();
+        arCanvas.SetActive(true);
+        uiCanvas.SetActive(false);
+
         
+    }
+    public void NoAr()
+    {
+        arCamera.PauseCamera();
+        arCanvas.SetActive(false);
+        uiCanvas.SetActive(true);
+
+
+    }
+
+    public void SetSalad()
+    {
+
+    }
+
+    public void SetWagyuJyu() 
+    {
+    }
+    
+    public void SetTata()
+    {
+
+    }
+
+    public void SetPlatter()
+    {
+
+    }
+
+    public void SetSearedWagyu()
+    {
+
     }
 
     // Update is called once per frame
