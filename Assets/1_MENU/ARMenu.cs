b8933f7fccab0d785855a9b83d39537a74ed9a05
curtislow777 <116061120+curtislow777@users.noreply.github.com>
2024-07-17 11:48:41 +0800

    menu testing

--- Assets/1_MENU/ARMenu.cs
+++ Assets/1_MENU/ARMenu.cs
@@ -15,9 +15,17 @@ public class ARMenu : MonoBehaviour
     public GameObject wagyuJyu;
     public GameObject salad;
 
+    public GameObject placementIndicator;
+    public GameObject placeButton;
+    public WorldTracker worldTracker;
+
     void Start()
     {
        arCamera = GetComponent<ARCamera>();
+       placementIndicator.SetActive(false);
+        worldTracker = GetComponent<WorldTracker>();
+        worldTracker.StopTracker();
+
     }
 
     public void ArMode()
@@ -26,8 +34,8 @@ public class ARMenu : MonoBehaviour
         arCanvas.SetActive(true);
         uiCanvas.SetActive(false);
 
-        
     }
+        
     public void NoAr()
     {
         arCamera.PauseCamera();
@@ -35,29 +43,67 @@ public class ARMenu : MonoBehaviour
         uiCanvas.SetActive(true);
 
 
+
     }
 
     public void SetSalad()
     {
+        salad.SetActive(true);
+        wagyuJyu.SetActive(false);
+        tata.SetActive(false);
+        beefPlatter.SetActive(false);
+        searedWagyu.SetActive(false);
+        uiCanvas.SetActive(false );
+        placementIndicator.SetActive(true);
+
 
     }
 
     public void SetWagyuJyu() 
     {
+        wagyuJyu.SetActive(true);
+        tata.SetActive(false);
+        beefPlatter.SetActive(false);
+        searedWagyu.SetActive(false);
+        salad.SetActive(false);
+        uiCanvas.SetActive(false);
+        placementIndicator.SetActive(true);
     }
-    
+
     public void SetTata()
     {
-
+        tata.SetActive(true);
+        beefPlatter.SetActive(false);
+        searedWagyu.SetActive(false);
+        salad.SetActive(false);
+        wagyuJyu.SetActive(false);
+        uiCanvas.SetActive(false);
+        placementIndicator.SetActive(true);
     }
 
     public void SetPlatter()
     {
+        beefPlatter.SetActive(true);
+        searedWagyu.SetActive(false);
+        salad.SetActive(false);
+        wagyuJyu.SetActive(false);
+        tata.SetActive(false);
+        uiCanvas.SetActive(false);
+        worldTracker.StartTracker();
+        placementIndicator.SetActive(true);
 
     }
 
     public void SetSearedWagyu()
     {
+        searedWagyu.SetActive(true);
+        salad.SetActive(false);
+        wagyuJyu.SetActive(false);
+        tata.SetActive(false);
+        beefPlatter.SetActive(false);
+        uiCanvas.SetActive(false);
+        placementIndicator.SetActive(true);
+        placeButton.SetActive(true);
 
     }
 
